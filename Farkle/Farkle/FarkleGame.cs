abda86da867cceb501e979a1dffacabd6bb973ce
Sam Cranford <me@nostradamnit.com>
2016-02-08 20:51:40 +0100

    6 tests passes

--- Farkle/Farkle/FarkleGame.cs
+++ Farkle/Farkle/FarkleGame.cs
@@ -1,16 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Farkle
 {
-	public class FarkleGame
+	public class ScoreCalculator
 	{
-		public FarkleGame ()
+		public ScoreCalculator ()
 		{
 		}
 
-		public List<int> RollDice(){
-			return new List<int> () { 1, 2, 2, 4, 5, 6 };
+		public List<int> ThrowDice(){
+			return new List<int> () { 1, 2, 2, 4, 3, 6 };
+		}
+
+		public bool RollHasOneOrFive(List<int> roll) {
+			return roll.Contains(1) || roll.Contains(5);
+		}
+
+		public bool RollHasTriple (List<int> rollValues)
+		{
+			return rollValues.GroupBy(value => value)
+						     .Any (g => g.Count() == 3);
+		}
+
+		public bool RollHasSextet (List<int> rollValues)
+		{
+			return rollValues.GroupBy(value => value)
+				.Any (g => g.Count() == 6);
 		}
 	}
 }
--- FarkleTests/FarlkleTest.cs
+++ FarkleTests/FarlkleTest.cs
@@ -10,19 +10,76 @@ namespace FarkleTests
 	public class FarkleTest
 	{
 		[Test()]
-		public void CanRollFirstSixDice ()
+		public void CanThrowSixDice ()
 		{
 			// arrange
-			var game = new FarkleGame ();
+			var scoreCalculator = new ScoreCalculator ();
 
 			// act
-			List<int> rollValues = game.RollDice ();
+			List<int> rollValues = scoreCalculator.ThrowDice ();
 
 			// assert
 			Assert.AreEqual (6, rollValues.Count);
+		}
 
+		[Test()]
+		public void ThrowDoesntHaveOneOrFive(){
+			// arrange
+			var scoreCalculator = new ScoreCalculator ();
 
+			// act
+			var rollValues = new List<int>  { 3, 2, 2, 4, 3, 6 };
+
+			// assert
+			Assert.IsFalse (scoreCalculator.RollHasOneOrFive(rollValues));
+		}
+
+		[Test()]
+		public void ThrowHasOne(){
+			// arrange
+			var scoreCalculator = new ScoreCalculator ();
+
+			// act
+			var rollValues = new List<int>  { 1, 2, 2, 4, 3, 6 };
+
+			// assert
+			Assert.IsTrue (scoreCalculator.RollHasOneOrFive(rollValues));
+		}
+
+		[Test()]
+		public void ThrowHasFive(){
+			// arrange
+			var scoreCalculator = new ScoreCalculator ();
+
+			// act
+			var rollValues = new List<int>  { 5, 2, 2, 4, 3, 6 };
+
+			// assert
+			Assert.IsTrue (scoreCalculator.RollHasOneOrFive(rollValues));
+		}
+
+		[Test()]
+		public void ThrowHasATriple(){
+			// arrange
+			var scoreCalculator = new ScoreCalculator ();
+
+			// act
+			var rollValues = new List<int>  { 2, 2, 2, 4, 3, 6 };
+
+			// assert
+			Assert.IsTrue (scoreCalculator.RollHasTriple(rollValues));
+		}
+		
+		[Test()]
+		public void ThrowHasASextet(){
+			// arrange
+			var scoreCalculator = new ScoreCalculator ();
+
+			// act
+			var rollValues = new List<int>  { 2, 2, 2, 2, 2, 2 };
+
+			// assert
+			Assert.IsTrue (scoreCalculator.RollHasSextet(rollValues));
 		}
 	}
 }
-
